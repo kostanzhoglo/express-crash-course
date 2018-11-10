1b21b0c656f00b703a7267bfa1f50cb05b6e31ee
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-10 15:10:11 -0500

    Create express basic server, get function and listen function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello you, over there, yes you!')
+const express = require('express');
+// import express from 'express';
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Yo hello McFly');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on Port 3000...')
+})
