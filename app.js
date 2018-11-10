714756ca5c94e4e79676841b15fd9c8a1c56e9be
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-10 15:25:22 -0500

    Add bodyParser middleware and set Static Path middleware in app.js

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const path = require('path');
 
 const app = express();
 
+// MIDDLEWARE
+// const logger = (req, res, next) => {
+//   console.log('Logging...');
+//   next();
+// }
+//
+// app.use(logger);
+
+// Body Parser MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlendcoded({extended: false}))
+
+// Set Static Path - static (or 'Public' folder) is for holding static files -- CSS files, jQuery, etc.
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
   res.send('Yo hello McFly');
 });
